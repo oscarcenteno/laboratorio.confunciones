5ab331eebfc0547ef5dbedd876210bbbf86d4cbd
Oscar <oscar.centeno@outlook.com>
2017-02-02 10:03:34 -0600

    Codigo inicial.

---
+++ Laboratorio.Negocio/3 Rendimientos por descuento/2 Con funciones/CalculosDeRendimiento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Negocio.RendimientosPorDescuento.ConFunciones
+{
+    public static class CalculosDeRendimiento
+    {
+        public static double GenereElRendimientoPorDescuento(double elValorFacial,
+                double elValorTransadoNeto,
+                double laTasaDeImpuesto,
+                DateTime laFechaDeVencimiento,
+                DateTime laFechaActual,
+                bool tieneTratamientoFiscal)
+        {
+            double elValorTransadoBruto;
+            if (tieneTratamientoFiscal)
+            {
+                TimeSpan elTiempoAlVencimiento;
+                elTiempoAlVencimiento = (laFechaDeVencimiento - laFechaActual);
+                double losDiasAlVencimiento;
+                losDiasAlVencimiento = elTiempoAlVencimiento.Days;
+
+                double laTasaNeta;
+                laTasaNeta = (elValorFacial - elValorTransadoNeto) / (elValorTransadoNeto * (losDiasAlVencimiento / 365)) * 100;
+                double laTasaBruta;
+                laTasaBruta = laTasaNeta / (1 - (laTasaDeImpuesto / 100));
+                elValorTransadoBruto = elValorFacial / (1 + ((laTasaBruta / 100) * (losDiasAlVencimiento / 365)));
+            }
+            else
+            {
+                elValorTransadoBruto = elValorTransadoNeto;
+            }
+
+            double elRendimiento = elValorFacial - elValorTransadoBruto;
+            return Math.Round(elRendimiento, 4);
+        }
+    }
+}
